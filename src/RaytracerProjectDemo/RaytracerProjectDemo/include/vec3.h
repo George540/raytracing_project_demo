8575815154bcd7fb8f7a0b428f30506f45bff766
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-08-29 20:29:01 -0400

    Add Utility Functions
    - Operators *, /,  <<, +
    - Dot, Cross products
    - Unit Vector

--- src/RaytracerProjectDemo/RaytracerProjectDemo/include/vec3.h
+++ src/RaytracerProjectDemo/RaytracerProjectDemo/include/vec3.h
@@ -9,11 +9,11 @@ using std::sqrt;
 class vec3
 {
 	public:
-		// Constructors & Destructors
+		// Constructors
 		vec3() : coords{0 ,0 ,0} {}
 		vec3(const float e0, const float e1, const float e2) : coords{e0, e1, e2} {}
-		vec3(const vec3& v) : coords{v.coords[0], v.coords[1], v.coords[2]} {}
-		~vec3() = default;
+		//vec3(const vec3& v) : coords{v.coords[0], v.coords[1], v.coords[2]} {}
+		//~vec3() = default;
 
 		// Coordinate Accessors
 		float x() const { return coords[0]; }
@@ -52,13 +52,67 @@ class vec3
 		{
 			return coords[0] * coords[0] + coords[1] * coords[1] + coords[2] * coords[2];
 		}
-		
-	public:
+
 		float coords[3];
 };
 
 // Type aliases for vec3
 using point3 = vec3;   // 3D point
 using color = vec3;    // RGB color
+
+// Utility Functions
+inline std::ostream& operator<<(std::ostream& out, const vec3& v)
+{
+	return out << v.coords[0] << ' ' << v.coords[1] << ' ' << v.coords[2];
+}
+
+inline vec3 operator+(const vec3& v, const vec3& u)
+{
+	return vec3(v.coords[0] + u.coords[0], v.coords[1] + u.coords[1], v.coords[2] + u.coords[2]);
+}
+
+inline vec3 operator-(const vec3& v, const vec3& u)
+{
+	return vec3(v.coords[0] - u.coords[0], v.coords[1] - u.coords[1], v.coords[2] - u.coords[2]);
+}
+
+inline vec3 operator*(const vec3& v, const vec3& u)
+{
+	return vec3(v.coords[0] * u.coords[0], v.coords[1] * u.coords[1], v.coords[2] * u.coords[2]);
+}
+
+inline vec3 operator*(float t, const vec3& v)
+{
+	return vec3(t * v.coords[0], t * v.coords[1], t * v.coords[2]);
+}
+
+inline vec3 operator*(const vec3& v, float t)
+{
+	return t * v;
+}
+
+inline vec3 operator/(vec3 v, float t)
+{
+	return (1 / t) * v;
+}
+
+inline float dot(const vec3& v, const vec3& u)
+{
+	return v.coords[0] * u.coords[0]
+		+ v.coords[1] * u.coords[1]
+		+ v.coords[2] * u.coords[2];
+}
+
+inline vec3 cross(const vec3& v, const vec3& u)
+{
+	return vec3(v.coords[1] * u.coords[2] - v.coords[2] * u.coords[1],
+		v.coords[2] * u.coords[0] - v.coords[0] * u.coords[2],
+		v.coords[0] * u.coords[1] - v.coords[1] * u.coords[0]);
+}
+
+inline vec3 unit_vector(vec3 v)
+{
+	return v / v.length();
+}
 
 #endif
